feb3bd34041217370b66a74d8784c51b4e946e9f
x2gut <zorganhux@gmail.com>
2025-06-10 00:19:12 +0200

    Tests added

---
+++ src/chat/test/chat.gateway.spec.ts
@@ -0,0 +1,75 @@
+import * as dotenv from 'dotenv';
+import { Server, Socket } from 'socket.io';
+import { ChatGateway } from '../chat.gateway';
+import { ChatService } from '../chat.service';
+import { ChatEvents } from '../events/chat-events.enum';
+
+dotenv.config({ path: '.env.dev' });
+
+describe('ChatGateway', () => {
+  let gateway: ChatGateway;
+  let serverMock: Pick<Server, 'emit' | 'to'>;
+  let chatServiceMock: Partial<ChatService>;
+  let clientMock: Partial<Socket>;
+
+  beforeEach(() => {
+    serverMock = {
+      to: jest.fn().mockReturnValue({ emit: jest.fn() }),
+      emit: jest.fn(),
+    };
+
+    clientMock = {
+      id: '',
+      data: {
+        roomId: 'test',
+        username: 'test',
+      },
+      emit: jest.fn(),
+      join: jest.fn(),
+      rooms: new Set<string>(),
+    };
+
+    chatServiceMock = {
+      handleUserConnectEvent: jest.fn(),
+      handleUserDisconnectEvent: jest.fn(),
+      getUsers: jest.fn(),
+    };
+
+    gateway = new ChatGateway(chatServiceMock as any);
+    (gateway.server as any) = serverMock as Server;
+  });
+
+  it('should send message when connected', async () => {
+    await gateway.handleJoin(clientMock as any, {
+      username: 'test',
+      roomId: 'test',
+    });
+
+    expect(chatServiceMock.handleUserConnectEvent).toHaveBeenCalled();
+    expect(serverMock.to('test').emit).toHaveBeenCalled();
+    expect(clientMock.join).toHaveBeenLastCalledWith('test');
+  });
+
+  it('should send new message to users in the room', async () => {
+    await gateway.handleMessage({
+      message: 'test',
+      roomId: 'test',
+      username: 'test',
+    });
+
+    expect(serverMock.to('test').emit).toHaveBeenCalledWith(
+      ChatEvents.NEW_MESSAGE,
+      expect.objectContaining({ type: 'user' }),
+    );
+  });
+
+  it('should disconnect user and send message', async () => {
+    await gateway.handleDisconnect(clientMock);
+
+    expect(chatServiceMock.handleUserDisconnectEvent).toHaveBeenCalled();
+    expect(serverMock.to('test').emit).toHaveBeenCalledWith(
+      ChatEvents.NEW_MESSAGE,
+      expect.objectContaining({ type: 'system' }),
+    );
+  });
+});
